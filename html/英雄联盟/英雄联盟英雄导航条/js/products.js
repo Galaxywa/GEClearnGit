090184058be41d13d46c753db2eb89fce6141e92
Galaxywar <11627718+galaxywar@user.noreply.gitee.com>
2022-09-19 16:20:36 +0800

    引入

---
+++ html/英雄联盟/英雄联盟英雄导航条/js/products.js
@@ -0,0 +1,83 @@
+var products = {
+
+
+  AllHeroes: [{
+        title: '黑暗之女',
+        img: 'https://game.gtimg.cn/images/lol/act/img/champion/Annie.png',
+      },
+      {
+        title: '迷失之牙',
+        img: 'https://game.gtimg.cn/images/lol/act/img/champion/Gnar.png',
+      },
+      {
+        title: '冰霜女巫',
+    
+        img: 'https://game.gtimg.cn/images/lol/act/img/champion/Lissandra.png',
+      },
+      {
+        title: '破败之王',
+      
+        img: 'https://game.gtimg.cn/images/lol/act/img/champion/Viego.png',
+      },
+      {
+
+        title: '荒漠屠夫',
+        img: 'https://game.gtimg.cn/images/lol/act/img/champion/Renekton.png',
+      },
+      {
+
+        title: '荣耀行刑官',
+      
+        img: 'https://game.gtimg.cn/images/lol/act/img/champion/Draven.png',
+      },
+      {
+        title: '深海泰坦',
+    
+      img: 'https://game.gtimg.cn/images/lol/act/img/champion/Nautilus.png',
+    },
+      
+    ],
+
+  warrior: [{
+      title: '迷失之牙',
+      img: 'https://game.gtimg.cn/images/lol/act/img/champion/Gnar.png',
+    },
+  ],
+
+  Mage: [{
+    title: '冰霜女巫',
+
+    img: 'https://game.gtimg.cn/images/lol/act/img/champion/Lissandra.png',
+  },
+],
+
+  assassin: [{
+  title: '破败之王',
+
+  img: 'https://game.gtimg.cn/images/lol/act/img/champion/Viego.png',
+},
+],
+
+  tank: [{
+
+  title: '荒漠屠夫',
+  img: 'https://game.gtimg.cn/images/lol/act/img/champion/Renekton.png',
+},
+],
+
+shooter: [{
+
+  title: '荣耀行刑官',
+
+  img: 'https://game.gtimg.cn/images/lol/act/img/champion/Draven.png',
+},
+],
+
+auxiliary: [{
+    title: '深海泰坦',
+
+  img: 'https://game.gtimg.cn/images/lol/act/img/champion/Nautilus.png',
+},
+],
+
+  };
---
+++ html/英雄联盟/英雄联盟英雄导航条/js/tabs.js
@@ -0,0 +1,29 @@
+var tabs = [{
+    title: '所有英雄',
+    type: 'AllHeroes'
+  },
+  {
+    title: '战士',
+    type: 'warrior'
+  },
+  {
+    title: '法师',
+    type: 'Mage'
+  },
+  {
+    title: '刺客',
+    type: 'assassin'
+  },
+  {
+    title: '坦克',
+    type: 'tank'
+  },
+  {
+    title: '射手',
+    type: 'shooter'
+  },
+  {
+    title: '辅助',
+    type: 'auxiliary'
+  }
+];
